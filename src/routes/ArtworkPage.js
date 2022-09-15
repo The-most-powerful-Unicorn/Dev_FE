2152c916e17bba3b418a5d4fa00b4c65fd8b99f5
44Chick <49892292+44Chick@users.noreply.github.com>
2022-09-15 15:49:51 +0900

    걸어보기 복구중 0.2

--- src/routes/ArtworkPage.js
+++ src/routes/ArtworkPage.js
@@ -47,7 +47,7 @@ function ArtworkPage() {
     // artworkRental.toLocaleString();
 
     const bgImgURL =
-      "../img/interior01Living.jpg";
+        "../img/interior01Living.jpg";
 
     const [preImg, setPreImg] = useState(bgImgURL);
 
@@ -77,11 +77,11 @@ function ArtworkPage() {
                     <div className="artworkInfo">
                         <div className="artworkInfo1">
                             <p className="artworkInfo1Left">작품정보</p>
-                            <div className="artworkInfo1Middle">{artworks.artworkMaterial}<br/>{artworks.artworkCmSize}({artworks.artworkSize}호), {artworks.artworkDate}
+                            <div className="artworkInfo1Middle">{artworks.artworkMaterial}<br />{artworks.artworkCmSize}({artworks.artworkSize}호), {artworks.artworkDate}
                             </div>
                             <div className="artworkInfo1Right">
-                                <a href=''><img src='../img/heartRound.png' style={{width:"40px", paddingRight:"8px"}}/></a>
-                                <a href=''><img src='../img/share.png' style={{width:"40px"}}/></a>
+                                <a href=''><img src='../img/heartRound.png' style={{ width: "40px", paddingRight: "8px" }} /></a>
+                                <a href=''><img src='../img/share.png' style={{ width: "40px" }} /></a>
                             </div>
                         </div>
                         <div>
@@ -114,90 +114,93 @@ function ArtworkPage() {
                     </div>
                 </div>
                 <div className="container">
-                <h2 className="head">인태리어 해보기</h2>
-                <div className="side preView"
-                    style={{ height: "100vh",
-                    width:"50vw",
-                    backgroundImage:`url(${preImg})` }}>
-                    <img
-                    style ={{height:"30vh",
-                }} 
-                    src={artworks.artworkImgUrl}  />
+                    <h2 className="head">인태리어 해보기</h2>
+                    <div className="side preView"
+                        style={{
+                            height: "100vh",
+                            width: "50vw",
+                            backgroundImage: `url(${preImg})`
+                        }}>
+                        <img
+                            style={{
+                                height: "30vh",
+                            }}
+                            src={artworks.artworkImgUrl} />
+                    </div>
+                    <div className="button_wrap">
+                        <div className="" >
+                            <button onClick={() => imgChange('../img/interior01Living.jpg')}>
+                                <img
+                                    className='button_interior'
+                                    src='../img/interior01Living.jpg' />
+                            </button>
+                        </div>
+                        <div className="" >
+                            <button onClick={() => imgChange('../img/interior02Shop.jpg')}>
+                                <img
+                                    className='button_interior'
+                                    src='../img/interior02Shop.jpg' />
+                            </button>
+                        </div>
+                        <div className="" >
+                            <button onClick={() => imgChange('../img/interior03Room.jpg')}>
+                                <img
+                                    className='button_interior'
+                                    src='../img/interior03Room.jpg' />
+                            </button>
+                        </div>
+                        <div className="" >
+                            <button onClick={() => imgChange('../img/interior04Desk.jpg')}>
+                                <img
+                                    className='button_interior'
+                                    src='../img/interior04Desk.jpg' />
+                            </button>
+                        </div>
+                        <div className="" >
+                            <button onClick={() => imgChange('../img/interior05Bed.jpg')}>
+                                <img
+                                    className='button_interior'
+                                    src='../img/interior05Bed.jpg' />
+                            </button>
+                        </div>
+                        <div className="" >
+                            <button onClick={() => imgChange('../img/interior06Bed2.jpg')}>
+                                <img
+                                    className='button_interior'
+                                    src='../img/interior06Bed2.jpg' />
+                            </button>
+                        </div>
+                        <div className="" >
+                            <button onClick={() => imgChange('../img/interior07Dining.jpg')}>
+                                <img
+                                    className='button_interior'
+                                    src='../img/interior07Dining.jpg' />
+                            </button>
+                        </div>
+                        <div className="" >
+                            <button onClick={() => imgChange('../img/interior08Office.jpg')}>
+                                <img
+                                    className='button_interior'
+                                    src='../img/interior08Office.jpg' />
+                            </button>
+                        </div>
+                        <div className="" >
+                            <button onClick={() => imgChange('../img/interior09Black.jpg')}>
+                                <img
+                                    className='button_interior'
+                                    src='../img/interior09Black.jpg' />
+                            </button>
+                        </div>
+
+                    </div>
                 </div>
-                <div className="button_wrap">
-                    <div className="" >
-                        <button onClick = { ()=> imgChange('../img/interior01Living.jpg')}>
-                            <img 
-                            className='button_interior' 
-                            src='../img/interior01Living.jpg' />
-                        </button>
-                    </div>
-                    <div className="" >
-                        <button onClick = { ()=> imgChange('../img/interior02Shop.jpg')}>
-                            <img 
-                            className='button_interior' 
-                            src='../img/interior02Shop.jpg' />
-                        </button>
-                    </div>
-                    <div className="" >
-                        <button onClick = { ()=> imgChange('../img/interior03Room.jpg')}>
-                            <img 
-                            className='button_interior' 
-                            src='../img/interior03Room.jpg' />
-                        </button>
-                    </div>
-                    <div className="" >
-                        <button onClick = { ()=> imgChange('../img/interior04Desk.jpg')}>
-                            <img 
-                            className='button_interior' 
-                            src='../img/interior04Desk.jpg' />
-                        </button>
-                    </div>
-                    <div className="" >
-                        <button onClick = { ()=> imgChange('../img/interior05Bed.jpg')}>
-                            <img 
-                            className='button_interior' 
-                            src='../img/interior05Bed.jpg' />
-                        </button>
-                    </div>
-                    <div className="" >
-                        <button onClick = { ()=> imgChange('../img/interior06Bed2.jpg')}>
-                            <img 
-                            className='button_interior' 
-                            src='../img/interior06Bed2.jpg' />
-                        </button>
-                    </div>
-                    <div className="" >
-                        <button onClick = { ()=> imgChange('../img/interior07Dining.jpg')}>
-                            <img 
-                            className='button_interior' 
-                            src='../img/interior07Dining.jpg' />
-                        </button>
-                    </div>
-                    <div className="" >
-                        <button onClick = { ()=> imgChange('../img/interior08Office.jpg')}>
-                            <img 
-                            className='button_interior' 
-                            src='../img/interior08Office.jpg' />
-                        </button>
-                    </div>
-                    <div className="" >
-                        <button onClick = { ()=> imgChange('../img/interior09Black.jpg')}>
-                            <img 
-                            className='button_interior' 
-                            src='../img/interior09Black.jpg' />
-                        </button>
-                    </div>
-                    
-                </div>
-            </div>
 
 
             </div>
             <div className="">{artworks.artworkInfo}</div>
 
         </div>
-        
+
     );
 }
 
